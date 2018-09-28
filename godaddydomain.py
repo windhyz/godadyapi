d8cbacf908b38812e0e02224e500cde3d36ef9b7
admin <windhyz@189.cn>
2018-09-28 17:36:21 +0800

    delete some annotations in godaddydomain.py

--- godaddydomain.py
+++ godaddydomain.py
@@ -15,8 +15,7 @@ except KeyboardInterrupt:
 
 if __name__ == "__main__":
 
-    #keydict = KEYDICT
-    #arglist = []
+
     opts = Options()
     parser = opts.parse_command()
     args = parser.parse_args()
@@ -31,38 +30,11 @@ if __name__ == "__main__":
     else:
         print("please input godday api domain")
         exit(0)
-    #if args.status:
-    #    arglist.append(args.status)
-    #if args.domain:
-    #    arglist.append(args.domain)
-    #if args.creatat:
-    #    arglist.append(args.creatat)
-    #if args.expires:
-    #    arglist.append(args.expires)
-    #if args.renewAuto:
-    #    arglist.append(args.renewAuto)
-    #if args.outfile:
-    #    arglist.append(args.outfile)
-    #if args.nameserver:
-    #    arglist.append(args.nameserver)
 
     url = URL
-    #print(arglist)
-    #if godaddyid in KEYDICT:
-    #    dodaddykey = KEYDICT[godaddyid]
-    #if godaddyid in SECRETDICT:
-    #    godaddysecret = SECRETDICT[godaddyid]
 
     gddomain = DodaddyDomain(args, url)
     if args.domain:
         gddomain.getOneDomaininfo(args.domain)
     else:
         gddomain.alldomaininfo()
-
-
-
-
-
-
-
-
